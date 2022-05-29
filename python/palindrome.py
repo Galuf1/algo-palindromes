4052d1ab1d568bf1dbc24b7d2b1215e17dd039a6
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-05-29 18:33:20 +0200

    python with 2 pointers

--- python/palindrome.py
+++ python/palindrome.py
@@ -1,2 +1,15 @@
 def palindrome(word):
-    # Write code here
+    # clean the word from spaces and puctuation and lowercase it
+    word_low = str(word).lower()
+    word_cleaned = "".join(x for x in word_low if x.isalnum())
+
+    # we keep 2 pointers moving in opposite directions during the length of the string, if any of them are not equal it is not a palindrome 
+    left = 0
+    right = -1
+    for i in range(len(word_cleaned)):
+        if word_cleaned[left] == word_cleaned[right]:
+            left +=1
+            right -=1
+        else:
+            return False
+    return True
